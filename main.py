3d22a8635f56a66071754b97489de63484cc1f77
Spencer Murray <spencer.murray@sentry.io>
2024-07-30 09:26:55 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def add(a: int, b: int) -> int:
+    return a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a + b
+
+def subtract(a: int, b: int) -> int:
+    return a - b
+
+def multiply(a: int, b:int) -> int:
+    return a * b
+
+def divide(a: int, b: int) -> float:
+    return a / b / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1
