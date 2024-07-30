c984785b4eb5e3f3ca5d8e05aa1bc62b5d53a8d5
Spencer Murray <spencer.murray@sentry.io>
2024-07-30 09:30:52 -0400

    Even bigger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def add(a: int, b: int) -> int:
-    return a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a + b
+    return a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a - a + b - b + a + b
 
 def subtract(a: int, b: int) -> int:
     return a - b
@@ -8,4 +8,4 @@ def multiply(a: int, b:int) -> int:
     return a * b
 
 def divide(a: int, b: int) -> float:
-    return a / b / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1
+    return a / b / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1 / 1
